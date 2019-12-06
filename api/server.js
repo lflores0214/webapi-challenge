009a034e0dab7bca9baed9ab6570753dfabeeb43
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-12-06 11:58:51 -0700

    added a react app to display data from backend

--- api/server.js
+++ api/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const helmet = require("helmet");
 const projectRouter = require("../Routes/projectRouter");
 const actionRouter = require("../Routes/actionRouter");
+const cors = require("cors");
 const server = express();
 
 server.use(logger);
 server.use(helmet());
 server.use(express.json());
+server.use(cors());
 server.use("/api/projects", projectRouter);
 server.use("/api/actions", actionRouter);
 
--- webapi-stretch/src/App.js
+++ webapi-stretch/src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/api/projects")
+      .then(response => {
+        console.log(response.data);
+        setProjects(response.data);
+        console.log(projects);
+      })
+      .catch(error => {
+        console.log("ERROR", error);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {projects.map(project => (
+        <div>
+          <h2>{project.name}</h2>
+          <p>{project.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
